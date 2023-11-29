1945ae2d8b0351ba9fd02069bd73ad18c7fc496f
Gabriel Amorim <gvfamorim.ntt@rd.com.br>
2023-11-29 18:50:59 -0300

    feature: implementing some initial features/tools for the challenge

---
+++ src/components/PlanetsList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <ul>
+        <li v-for="planet in planets" :key="planet.name">{{ planet.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        planets: []
+      };
+    },
+    mounted() {
+      axios.get('https://swapi.dev/api/planets/')
+        .then(response => {
+          this.planets = response.data.results;
+        });
+    }
+  };
+  </script>
+  
