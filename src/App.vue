abfb68f466fc822340e028627a58bdfc7742387b
Gabriel Amorim <gvfamorim.ntt@rd.com.br>
2023-11-29 18:54:15 -0300

    feature: Adding some styles of bootstrap and sorting of the planets

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script>
 import PlanetsList from './components/PlanetsList.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default {
   name: 'App',
--- src/components/PlanetsList.vue
+++ src/components/PlanetsList.vue
@@ -1,26 +1,68 @@
 <template>
-    <div>
-      <ul>
-        <li v-for="planet in planets" :key="planet.name">{{ planet.name }}</li>
+  <div class="container mt-3">
+    <h2>Star Wars Planets</h2>
+
+    <div v-if="loading">
+      Loading planets...
+    </div>
+
+    <div v-else>
+      <div class="mb-3">
+        <label for="sort-select" class="form-label">Sort by:</label>
+        <select id="sort-select" class="form-select" v-model="sortKey" @change="sortPlanets">
+          <option value="name">Name</option>
+          <option value="population">Population</option>
+          <option value="diameter">Diameter</option>
+        </select>
+      </div>
+
+      <ul class="list-group">
+        <li v-for="planet in planets" :key="planet.name" class="list-group-item">
+          {{ planet.name }} - Population: {{ planet.population }}, Diameter: {{ planet.diameter }}
+        </li>
       </ul>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        planets: []
-      };
-    },
-    mounted() {
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      planets: [],
+      loading: false,
+      sortKey: 'name',
+    };
+  },
+  mounted() {
+    this.fetchPlanets();
+  },
+  methods: {
+    fetchPlanets() {
+      this.loading = true;
       axios.get('https://swapi.dev/api/planets/')
         .then(response => {
           this.planets = response.data.results;
+          this.sortPlanets();
+        })
+        .catch(error => {
+          console.error("Error fetching planets:", error);
+        })
+        .finally(() => {
+          this.loading = false;
         });
+    },
+    sortPlanets() {
+      this.planets.sort((a, b) => {
+        if (this.sortKey === 'population' || this.sortKey === 'diameter') {
+          return parseInt(a[this.sortKey]) - parseInt(b[this.sortKey]);
+        }
+        return a[this.sortKey].localeCompare(b[this.sortKey]);
+      });
     }
-  };
-  </script>
+  }
+};
+</script>
   
